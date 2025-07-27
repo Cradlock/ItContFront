9b80daadacc4a84661a3b641d702a6bd4b0d52ee
Your Name <you@example.com>
2025-07-27 16:53:32 +0600

    add adapt on Page Profile footer 1

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,12 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="footer-component">
-        <div className="container">
-          <div className="footer-nav">
-            <p>© 2025</p>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">It-Acodemy ---- link to contacts</a>
-          </div>
-        </div>
+
       </footer>
     );
   }
